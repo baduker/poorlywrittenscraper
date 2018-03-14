53e3617543247a84959c09ac89d51d7c58d76bec
baduker <baduker@protonmail.ch>
2018-03-14 19:24:36 +0100

    First draft of the script; it just gets all the links

---
+++ poorlywrittenscraper.py
@@ -0,0 +1,25 @@
+import re
+import requests
+from bs4 import BeautifulSoup as bs
+
+url = 'http://www.poorlydrawnlines.com/archive/'
+req = requests.get(url)
+page = req.text
+
+soup = bs(page, 'html.parser')
+
+all_links = []
+
+for link in soup.find_all('a'):
+	all_links.append(link.get('href'))
+
+print('Finished downloading links!')
+print(len(all_links))
+
+pattern = re.compile(r'(http[s]?://?[^/\s]+/)(comic\/)((\w+-\w+-\w+-\w+\/)*)((\w+-\w+-\w+\/)?)((\w+\/)?)((\w+-\w+\/)?)')
+
+filtered = [i for i in all_links if pattern.match(i)]
+
+print(len(filtered))
+for i in filtered:
+	print(i)
