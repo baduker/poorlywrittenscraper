c6cf22220d1070d52d5295a33833cc643623214f
baduker <baduker@protonmail.ch>
2018-03-14 23:12:57 +0100

    Added function to make pdl dir

--- poorlywrittenscraper.py
+++ poorlywrittenscraper.py
@@ -1,8 +1,23 @@
+import os
 import re
 import requests
 import itertools
 from requests import get
 from bs4 import BeautifulSoup as bs
+
+HOME_DIR = os.getcwd()
+DEFAULT_DIR_NAME = 'poorlydrawnlines'
+
+def move_to_dir(title):
+  if os.getcwd() != HOME_DIR:
+    os.chdir(HOME_DIR)
+  try:
+    os.mkdir(title)
+    os.chdir(title)
+  except FileExistsError:
+    os.chdir(title)
+  except:
+    print("Couldn't create directory!")
 
 def grab_link(array):
   for link in itertools.islice(array, 0, 3):
@@ -38,6 +53,7 @@ pattern = re.compile(r'http://www.poorlydrawnlines.com/comic/.+')
 filtered_links = [i for i in all_links if pattern.match(i)]
 
 for link in grab_link(filtered_links):
+  move_to_dir(DEFAULT_DIR_NAME)
   url = grab_image_source(link)
   download_image(url)
 
