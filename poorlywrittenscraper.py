da3bd4694c0f8f018fae95b75410029088cf8b5c
baduker <baduker@protonmail.ch>
2018-03-14 22:43:30 +0100

    Added a generator and a function that grabs img urls

--- poorlywrittenscraper.py
+++ poorlywrittenscraper.py
@@ -1,6 +1,19 @@
 import re
 import requests
+import itertools
 from bs4 import BeautifulSoup as bs
+
+def grab_link(array):
+	for link in itertools.islice(array, 0, 3):
+		yield link
+
+def grab_image(link):
+	req = requests.get(link)
+	comic = req.text
+	soup = bs(comic, 'html.parser')
+	for i in soup.find_all('p'):
+		for img in i.find_all('img', src=True):
+			print(img['src'])
 
 url = 'http://www.poorlydrawnlines.com/archive/'
 req = requests.get(url)
@@ -13,13 +26,11 @@ all_links = []
 for link in soup.find_all('a'):
 	all_links.append(link.get('href'))
 
-print('Finished downloading links!')
-print(len(all_links))
-
 pattern = re.compile(r'http://www.poorlydrawnlines.com/comic/.+')
 
-filtered = [i for i in all_links if pattern.match(i)]
+filtered_links = [i for i in all_links if pattern.match(i)]
 
-print(len(filtered))
-for i in filtered:
-	print(i)
+for link in grab_link(filtered_links):
+	grab_image(link)
+
+# print('{} comics found.'.format(len(filtered_links)))
