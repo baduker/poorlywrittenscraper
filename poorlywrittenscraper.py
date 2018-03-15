a3f183c3024762878d6bbffd217f3e94e3cd0e9e
baduker <baduker@protonmail.ch>
2018-03-15 13:58:35 +0100

    Added logo, handle_menu function, and end message.

--- poorlywrittenscraper.py
+++ poorlywrittenscraper.py
@@ -9,6 +9,47 @@ from bs4 import BeautifulSoup as bs
 HOME_DIR = os.getcwd()
 DEFAULT_DIR_NAME = 'poorly_created_folder'
 
+def show_logo():
+  print("""
+a Python comic(al) scraper for poorlydwarnlines.com
+                         __
+.-----.-----.-----.----.|  |.--.--.
+|  _  |  _  |  _  |   _||  ||  |  |
+|   __|_____|_____|__|  |__||___  |
+|__|                        |_____|
+                __ __   __
+.--.--.--.----.|__|  |_|  |_.-----.-----.
+|  |  |  |   _||  |   _|   _|  -__|     |
+|________|__|  |__|____|____|_____|__|__|
+
+.-----.----.----.---.-.-----.-----.----.
+|__ --|  __|   _|  _  |  _  |  -__|   _|
+|_____|____|__| |___._|   __|_____|__|
+                      |__|
+version: 0.2 | author: baduker | https://github.com/baduker
+  """)
+
+def handle_menu():
+  print("\nThe scraper has found {} comics.".format(len(found_comics)))
+  print("How many comics do you want to download?")
+  print("Type 0 to exit.")
+
+  while True:
+    try:
+      global n_of_comics
+      n_of_comics = int(input(">> ").strip())
+    except ValueError:
+      print("Error: incorrect value. Try again.")
+      continue
+    if n_of_comics > len(found_comics) or n_of_comics < 0:
+      print("Error: incorrect number of comics to download. Try again.")
+      continue
+    elif n_of_comics == 0:
+      sys.exit()
+    else:
+      break
+  return n_of_comics
+
 def move_to_dir(title):
   if os.getcwd() != HOME_DIR:
     os.chdir(HOME_DIR)
@@ -53,30 +94,18 @@ def filter_archive(archive):
   filtered_links = [i for i in archive if pattern.match(i)]
   return filtered_links
 
+show_logo()
+
 all_comics = fetch_archive()
 found_comics = filter_archive(all_comics)
 
-print("{} comics found.".format(len(found_comics)))
-print("How many comics do you want to download?")
-
-while True:
-  try:
-    n_of_comics = int(input(">> ").strip())
-  except ValueError:
-    print("Error: incorrect value. Try again.")
-    continue
-  if n_of_comics > len(found_comics) or n_of_comics < 0:
-    print("Error: incorrect number of comics to download. Try again.")
-    continue
-  elif n_of_comics == 0:
-    sys.exit()
-  else:
-    break
+handle_menu()
 
 for link in grab_link(found_comics, n_of_comics):
   print("Downloading: {}".format(link))
   move_to_dir(DEFAULT_DIR_NAME)
   url = grab_image_source(link)
   download_image(url)
-  print("Done.")
+
+print("Successfully downloaded {} comics.".format(n_of_comics))
 
